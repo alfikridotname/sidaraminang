cf62257b0f2cd6164bc1a2f5730337eb4aab9717
alfikridotname <alfikri.name@gmail.com>
2020-08-03 12:04:54 +0700

    update check session MY Controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -6,7 +6,7 @@ class My_Controller extends MX_Controller
   public function __construct()
   {
     parent::__construct();
-    if (!$this->session->userdata('nama_lengkap') && $this->router->fetch_class() != 'auth') :
+    if (!$this->session->userdata('nama_lengkap') && $this->router->fetch_class() != 'auth' && $this->router->fetch_class() != 'registrasi') :
       redirect('auth');
 
     endif;
